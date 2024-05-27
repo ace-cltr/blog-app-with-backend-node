6df54d268b58a3456207596da996a6719aa1c4ff
ace-cltr <anand.vish201@gmail.com>
2024-05-27 16:43:05 +0530

    BE for blog-app

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const routes = require("./Routes/routes");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/", routes);
+
+const port = 5000;
+
+app.listen(port, () => {
+  console.log("your server is running on port " + port);
+});
